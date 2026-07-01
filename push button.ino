71eebabc7d2354b8814d853a8cbc9356c14a4e7a
Nishant <25211a04g4@bvrit.ac.in>
2026-07-01 23:02:35 +0530

    Add files via upload

---
+++ push button.ino
@@ -0,0 +1,15 @@
+int ledpin=11;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledpin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(ledpin,HIGH);
+ delay(1000);
+ digitalWrite(ledpin,LOW);
+ delay(1000);
+}
